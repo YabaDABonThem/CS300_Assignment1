8edf7e842b7337f6c48b1ba8bba7c9207f9cb8fc
Allen Bao <allenbao@uw.edu>
2022-10-05 12:35:43 -0700

    magic bag done

--- MagicBag.h
+++ MagicBag.h
@@ -4,42 +4,14 @@
 using namespace std;
 
 template<class T>
-class MagicBag : public PlainBag {
-    private:
-        bool removed = false;
+class MagicBag : public PlainBag<T> {
     public:
+        MagicBag()
+        {
+
+        }
         void print() const {
-            cout << "[]";
+            cout << "\n[]";
         }
 
-        void size() const {
-            if(!removed) {
-                return bagSize;
-            }
-            return 0;
-        }
-
-        bool remove(const T& item) {
-            // just return false if we don't even have the item
-            if (!contains(item)) return false;
-            removed = true;
-            // We need to find the item in the array and remove it
-            // basically what we can do is to assign the value of the last item
-            // to the value of the item being replaced and decrease the size
-            contents[indexOf(item)] = contents[bagSize-1];
-            --bagSize; // decrease size of bag if we do remove an item
-            return true;
-        }
-
-        int count(const T& item) const {
-            if (!removed) return 0;
-            // query the bag and find the count of the amount of items there are
-            int num = 0;
-            for (int i = 0; i < bagSize; ++i) {
-                if (contents[i] == item) { // is this the right comparator?
-                    ++num;
-                }
-            }
-            return num;
-        }
 };
